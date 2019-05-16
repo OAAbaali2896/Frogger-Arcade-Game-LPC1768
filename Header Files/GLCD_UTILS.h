a5766500241e70c1b2704a35e391b61ed6a3bc24
OAAbaali2896 <40522456+OAAbaali2896@users.noreply.github.com>
2019-05-16 12:05:22 -0400

    Add files via upload
    Header Files for the Frogger Game

---
+++ Header Files/GLCD_UTILS.h
@@ -0,0 +1,9 @@
+#ifndef _GLCD_UTILS_H
+#define _GLCD_UTILS_H
+
+extern void GLCD_draw_circle( int x0, int y0, int radius);
+extern void GLCD_draw_circle_full( int x0, int y0, int radius);
+extern void GLCD_draw_line( int x0, int y0, int x1, int y1);
+extern void GLCD_draw_smiley_face( int x0, int y0, int radius);
+
+#endif
---
+++ Header Files/type.h
@@ -0,0 +1,20 @@
+#ifndef __TYPE_H__
+#define __TYPE_H__
+
+/***********************************************************
+* This file is useful for Linux.                           *
+* Do not copy this over into the Keil uVision environment. *
+*  - use a version sch as                                  *
+*     C:\Keil\ARM\Boards\Keil\MCB1700\USBMem\type.h        *
+***********************************************************/
+
+typedef signed char        S8;
+typedef unsigned char      U8;
+typedef signed short       S16;
+typedef unsigned short     U16;
+typedef signed int         S32;
+typedef unsigned int       U32;
+typedef signed long long   S64;
+typedef unsigned long long U64;
+
+#endif
